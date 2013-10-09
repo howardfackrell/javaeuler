e5a3b12720fa0e912eacf3bd0cc05c7b02ead22e
howardfackrell <howard.fackrell@octanner.com>
2013-10-09 13:58:20 -0600

    modified test

--- src/test/java/com/hlf/javaeuler/euler1/Euler1Test.java
+++ src/test/java/com/hlf/javaeuler/euler1/Euler1Test.java
@@ -14,8 +14,9 @@ import static org.testng.Assert.assertEquals;
 public class Euler1Test {
     @Test
     public void testFindMultiplesBelow() throws Exception {
-
         Euler1 euler1 = new Euler1();
         assertEquals(euler1.findMultiplesBelow(10, 3, 5), 23, "Multiples of 3 and 5 below 10 should sum up to 23");
+        assertEquals(euler1.findMultiplesBelow(20, 3, 5), 78, "Multiples of 3 and 5 below 20 should sum up to 78");
+        assertEquals(euler1.findMultiplesBelow(0, 3, 5), 0, "Multiples of 3 and 5 below 20 should sum up to 0");
     }
 }
